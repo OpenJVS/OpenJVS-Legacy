d6841c7abea0fa8c5c51edaba9b640f84be1901d
Robert Dilley <rad561@student.bham.ac.uk>
2018-10-29 19:16:09 +0000

    Change serial settings

--- src/Utilities.c
+++ src/Utilities.c
@@ -5,7 +5,7 @@ int set_interface_attribs(int fd, int speed) {
     struct termios tty;
     memset( & tty, 0, sizeof tty);
     if (tcgetattr(fd, & tty) != 0) {
-        printf("JVSE: error %d from tcgetattr", errno);
+        printf("JVSE: Error %d from tcgetattr", errno);
         return -1;
     }
 
@@ -16,17 +16,19 @@ int set_interface_attribs(int fd, int speed) {
     tty.c_iflag &= ~IGNBRK;
     tty.c_lflag = 0;
     tty.c_oflag = 0;
-    tty.c_cc[VMIN] = 1;
-    tty.c_cc[VTIME] = 0;
+    tty.c_cc[VMIN] = 0;
+    tty.c_cc[VTIME] = 10;
     tty.c_iflag &= ~(IXON | IXOFF | IXANY);
     tty.c_cflag |= (CLOCAL | CREAD);
     tty.c_cflag &= ~(PARENB | PARODD);
     tty.c_cflag &= ~CSTOPB;
     tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CS8;
+    tty.c_cflag |= (CS8 | MDMBUF);
 
     tty.c_oflag &= ~OPOST;
+    tty.c_lflag |= (PENDIN | NOKERNINFO);
     tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
 
 
     if (tcsetattr(fd, TCSANOW, & tty) != 0) {
