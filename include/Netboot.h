7f1d8036485cb98b7ad9bad30e3786495d23be3f
Bobby Dilley <bobbydilley@live.com>
2019-01-14 14:44:25 +0000

    Added initial crc check on netboot

--- include/Netboot.h
+++ include/Netboot.h
@@ -18,6 +18,17 @@
 #include "Control.h"
 #include "Config.h"
 
+typedef long long		INT_64;
+typedef unsigned long long	UINT_64;
+typedef int 			INT_32;
+typedef unsigned int 		UINT_32;
+typedef short			INT_16;
+typedef unsigned short		UINT_16;
+typedef char 			INT_8;
+typedef unsigned char 		UINT_8;
+
+extern UINT_32 crc32(UINT_32 crc, const void *buf, UINT_32 size);
+
 int initNetboot();
 void runNetboot();
 void closeNetboot();
